4cf1cb1f3741774fd9a67186051f7e12822a8fcf
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-02 18:41:07 +0000

    Completed Feature Set - View All Grades

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import Header from './header';
+import GradeTable from './gradetable';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+  }
+  componentDidMount() {
+    this.getAllGrades();
+  }
+  getAllGrades() {
+    fetch('/api/grades')
+      .then(result => result.json())
+      .then(result => {
+        this.setState({ grades: result });
+      });
+  }
   render() {
-    return null;
+    return (
+      <div className = "container-fluid">
+        <Header text = "Student Grade Table" />
+        <GradeTable grades = { this.state.grades } />
+      </div>
+    );
   }
 }
 
