11e5d6da62de7a7c46361f367b120effc783f3d7
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-02 19:37:06 +0000

    Completed Feature-Set View Average Grades

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1 className = "mt-3 mb-3"> { this.props.text } </h1>
+      <div className = "container-fluid p-0 d-flex justify-content-between align-items-center mt-2">
+        <h1>{ this.props.text }</h1>
+        <h3 className="mt-2">Average Grade <span className = "badge badge-primary p-2"> { this.props.average }</span></h3>
+      </div>
     );
   }
 }
