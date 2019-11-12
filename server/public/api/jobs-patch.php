c7863044f4efa6ed179325755c95cfc2d98ffade
aaronmdomingo <aaronmdomingo@gmail.com>
2019-11-12 04:59:39 +0000

    added job-patch endpoint and tested that it works properly

---
+++ server/public/api/jobs-patch.php
@@ -0,0 +1,40 @@
+<?php
+    $bodyData = getBodyData();
+    $user_name = $bodyData["userName"];
+    $company = $bodyData["company"];
+    $status = $bodyData["status"];
+    $comments = $bodyData["comments"];
+    $date = $bodyData["date"];
+    $id = $bodyData["id"];
+
+    $filter_user_name = str_replace("'","\'", $user_name);
+    $filter_company = str_replace("'","\'", $company);
+    $filter_status = str_replace("'","\'", $status);
+    $filter_comments = str_replace("'","\'", $comments);
+
+    $getQuery = "SELECT * FROM `user` WHERE `user`.`user_name` = '$filter_user_name' ";
+    $patchQuery = "UPDATE `jobs`
+                SET `company` = '$filter_company', `status` = '$filter_status',
+                    `comments` = '$filter_comments'
+                    WHERE `id` = '$id' ";
+
+    $getResult = mysqli_query($conn, $getQuery);
+
+    if (!$getResult) {
+        throw new Exception('Username does not exist!');
+    }
+
+    $patchResult = mysqli_query($conn, $patchQuery);
+
+    if(!$patchResult) {
+        throw new Exception('Adding Entry failed');
+    } else {
+        $output = [
+            "success" => true
+        ];
+        $json_output = json_encode($output);
+        print($json_output);
+    }
+
+
+?>
