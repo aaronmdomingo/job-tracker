db104c6395f9ba597d46c81fff11fddfe524d501
aaronmdomingo <aaronmdomingo@gmail.com>
2019-11-13 22:09:47 +0000

    Added fetch method to check to see if user input matches whats on the database

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -28,12 +28,22 @@ class LogIn extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // const userObj = {
-    //   userName: this.state.userName,
-    //   passWord: this.state.passWord
-    // };
 
-    this.clearInputs();
+    fetch(`/api/user-login.php?userName=${this.state.userName}&passWord=${this.state.passWord}`)
+      .then(result => result.json())
+      .then(result => {
+        if (!result.error) {
+          const userObj = {
+            userName: `${this.state.userName}`
+          };
+          this.props.setCurrentUser(userObj);
+          this.props.logInUser();
+          this.props.history.push(`/dashboard/${this.state.userName}`);
+        } else {
+          this.setState({ fetchResponse: result.error });
+          this.clearInputs();
+        }
+      });
   }
 
   clearInputs() {
@@ -43,12 +53,12 @@ class LogIn extends React.Component {
     });
   }
   render() {
-    const { userName, passWord } = this.state;
+    const { userName, passWord, fetchResponse } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit} className="login__page container-fluid h-100 w-100 p3 d-flex flex-column align-items-center justify-content-center">
         <h1 className="display-2 m-3"> Tracker </h1>
-        <div className="login__container container-fluid w-75 h-50 d-flex flex-column align-items-center justify-content-center rounded">
+        <div className="login__container container-fluid w-75 h-50 d-flex flex-column align-items-center justify-content-center rounded fadeIn">
           <div className="container-fluid h-25 w-100 d-flex justify-content-center align-items-center">
             <input type="text" value={userName} name="username" onChange={this.handleChange} placeholder="Username" className="login__input text-center" autoComplete="off" required/>
           </div>
@@ -56,10 +66,10 @@ class LogIn extends React.Component {
             <input type="password" value={passWord} name="password" onChange={this.handleChange} placeholder="Password" className="login__input text-center" required/>
           </div>
           <div className="container-fluid h-25 w-100 d-flex justify-content-center align-items-center">
-
+            <p className="text-danger "> { fetchResponse } </p>
           </div>
         </div>
-        <button type="submit" className="btn btn-light m-3">Submit</button>
+        <button type="submit" className="btn btn-light m-3"> Submit</button>
       </form>
     );
   }
