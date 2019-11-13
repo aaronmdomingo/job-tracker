5efe8b0b45f8c8a4c2844c0fe7d68b17491249d5
aaronmdomingo <aaronmdomingo@gmail.com>
2019-11-13 00:06:29 +0000

    Added a new modal component that renders after a user clicks the delete button from the job component, added with fade in animation

---
+++ client/components/delete-modal.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class DeleteModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  componentWillMount() {
+    this.setState({ visible: true });
+  }
+  componentWillUnmount() {
+    this.setState({ visible: false });
+  }
+  handleDelete() {
+    this.props.deleteJob({ id: this.props.jobId });
+    this.props.hideDeleteModal();
+  }
+  handleClose() {
+    this.props.hideDeleteModal();
+  }
+  render() {
+    let modalClass;
+    const { visible } = this.state;
+
+    if (visible) {
+      modalClass = 'fadeIn';
+    } else {
+      modalClass = 'fadeOut';
+    }
+
+    return (
+      <div className={`modal ${modalClass}`} role="dialog">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.handleClose}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              You are about to delete this job, are you sure?
+            </div>
+            <div className="modal-footer w-100 d-flex justify-content-around align-items-center">
+              <button type="button" className="btn-mini btn-light" data-dismiss="modal" onClick={this.handleDelete}>Yes</button>
+              <button type="button" className="btn-mini btn-danger" onClick={this.handleClose}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DeleteModal;
