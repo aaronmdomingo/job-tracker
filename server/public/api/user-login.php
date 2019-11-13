b2d87d67012969b3575a0ce737f8e9a98f3fdb27
aaronmdomingo <aaronmdomingo@gmail.com>
2019-11-13 21:46:51 +0000

    Added login endpoint that checks for a match for both username and password

--- server/public/api/user-login.php
+++ server/public/api/user-login.php
@@ -1,10 +1,38 @@
 <?php
 
+  require_once('functions.php');
+  set_exception_handler('error_handler');
+  require_once('db-connection.php');
+  startup();
 
+  $userName = $_GET['userName'];
+  $passWord = $_GET['passWord'];
 
+  $query = "SELECT * FROM `user` WHERE `user_name` = '$userName' ";
 
+  $getResult = mysqli_query($conn, $query);
+  $getOutput = [];
 
-
-
+  if ($getResult) {
+    if (mysqli_num_rows($getResult) > 0) {
+      while ($row = mysqli_fetch_assoc($getResult)) {
+        $getOutput[] = $row["password"];
+      }
+      $hashedPassWord = $getOutput[0];
+      if (password_verify($passWord, $hashedPassWord)) {
+        $output = [
+            "success" => true
+        ];
+        $json_output = json_encode($output);
+        print($json_output);
+      } else {
+        throw new Exception("Incorrect Information");
+      }
+    } else {
+      throw new Exception("Incorrect Information");
+    }
+  } else {
+    throw new Exception("Query was not sucessful");
+  }
 
 ?>
